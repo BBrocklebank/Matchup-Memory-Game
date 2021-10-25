854b0283421950099c2fff1ad64aeaca2b081e39
Benjamin Brocklebank <benjamin.brocklebank@aol.com>
2021-10-25 19:23:01 +0000

    Feat: Created card array

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,54 @@
- 
+ document.addEventListener('DOMContentLoaded', () => {
+
+    // Cards included in this level
+    let cardArray = [
+        {
+            name: '1',
+            img: '../images/1_card.png'
+        },
+        {
+            name: '1',
+            img: '../images/1_card.png'
+        },
+        {
+            name: '2',
+            img: '../images/2_card.png'
+        },
+        {
+            name: '2',
+            img: '../images/2_card.png'
+        },
+        {
+            name: '3',
+            img: '../images/3_card.png'
+        },
+        {
+            name: '3',
+            img: '../images/3_card.png'
+        },
+        {
+            name: '4',
+            img: '../images/4_card.png'
+        },
+        {
+            name: '4',
+            img: '../images/4_card.png'
+        },
+        {
+            name: '5',
+            img: '../images/5_card.png'
+        },
+        {
+            name: '5',
+            img: '../images/5_card.png'
+        },
+        {
+            name: '6',
+            img: '../images/6_card.png'
+        },
+        {
+            name: '6',
+            img: '../images/6_card.png'
+        },
+    ]
+ })
