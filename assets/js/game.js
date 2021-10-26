0af36aea0195ee40b0d2d96935a5cb3f6526b5b2
Benjamin Brocklebank <82475611+BBrocklebank@users.noreply.github.com>
2021-10-26 19:50:03 +0100

    Bug: Image locations on disk corrected

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,51 +4,51 @@
     let cardArray = [
         {
             name: '1',
-            img: '../images/1_card.png'
+            img: '../Matchup-Memory-Game/assets/images/1_card.png'
         },
         {
             name: '1',
-            img: '../images/1_card.png'
+            img: '../Matchup-Memory-Game/assets/images/1_card.png'
         },
         {
             name: '2',
-            img: '../images/2_card.png'
+            img: '../Matchup-Memory-Game/assets/images/2_card.png'
         },
         {
             name: '2',
-            img: '../images/2_card.png'
+            img: '../Matchup-Memory-Game/assets/images/2_card.png'
         },
         {
             name: '3',
-            img: '../images/3_card.png'
+            img: '../Matchup-Memory-Game/assets/images/3_card.png'
         },
         {
             name: '3',
-            img: '../images/3_card.png'
+            img: '../Matchup-Memory-Game/assets/images/3_card.png'
         },
         {
             name: '4',
-            img: '../images/4_card.png'
+            img: '../Matchup-Memory-Game/assets/images/4_card.png'
         },
         {
             name: '4',
-            img: '../images/4_card.png'
+            img: '../Matchup-Memory-Game/assets/images/4_card.png'
         },
         {
             name: '5',
-            img: '../images/5_card.png'
+            img: '../Matchup-Memory-Game/assets/images/5_card.png'
         },
         {
             name: '5',
-            img: '../images/5_card.png'
+            img: '../Matchup-Memory-Game/assets/images/5_card.png'
         },
         {
             name: '6',
-            img: '../images/6_card.png'
+            img: '../Matchup-Memory-Game/assets/images/6_card.png'
         },
         {
             name: '6',
-            img: '../images/6_card.png'
+            img: '../Matchup-Memory-Game/assets/images/6_card.png'
         }
     ]
 
