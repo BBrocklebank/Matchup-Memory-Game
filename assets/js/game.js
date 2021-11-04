07fdc5340bed6f8185ca17301caa4022119bb050
Benjamin Brocklebank <82475611+BBrocklebank@users.noreply.github.com>
2021-11-04 14:17:36 +0000

    Feat: Added thrid level of difficulty

--- assets/js/game.js
+++ assets/js/game.js
@@ -27,7 +27,7 @@ document.addEventListener('DOMContentLoaded', () => {
     //  startButton.addEventListener('click', timer); //Are names an issue? same variable and function names 
     levelOne.addEventListener('click', difOne);
     levelTwo.addEventListener('click', difTwo);
-    //  levelThree.addEventListener('click', difThree);
+    levelThree.addEventListener('click', difThree);
 
     $(menu).append(startButton, levelOne, levelTwo, levelThree);
 
@@ -150,7 +150,7 @@ function difOne() {
         selectedCardsId = [];
 
         if (cardsMatched.length === cardArray.length / 2) {
-            scoreDisplay.textContent = " Congratulations! You've matched them all!";
+            scoreDisplay.textContent = " Congratulations! You've completed difficulty level 1!";
         }
     }
 
@@ -322,7 +322,177 @@ function difTwo() {
         selectedCardsId = [];
 
         if (cardsMatched.length === cardArray.length / 2) {
-            scoreDisplay.textContent = " Congratulations! You've matched them all!";
+            scoreDisplay.textContent = " Congratulations! You've completed difficulty level 2!";
+        }
+    }
+
+}
+
+function difThree() {
+
+    // Cards Array
+    let cardArray = [{
+            name: '1',
+            img: '../Matchup-Memory-Game/assets/images/1_card.png'
+        },
+        {
+            name: '1',
+            img: '../Matchup-Memory-Game/assets/images/1_card.png'
+        },
+        {
+            name: '2',
+            img: '../Matchup-Memory-Game/assets/images/2_card.png'
+        },
+        {
+            name: '2',
+            img: '../Matchup-Memory-Game/assets/images/2_card.png'
+        },
+        {
+            name: '3',
+            img: '../Matchup-Memory-Game/assets/images/3_card.png'
+        },
+        {
+            name: '3',
+            img: '../Matchup-Memory-Game/assets/images/3_card.png'
+        },
+        {
+            name: '4',
+            img: '../Matchup-Memory-Game/assets/images/4_card.png'
+        },
+        {
+            name: '4',
+            img: '../Matchup-Memory-Game/assets/images/4_card.png'
+        },
+        {
+            name: '5',
+            img: '../Matchup-Memory-Game/assets/images/5_card.png'
+        },
+        {
+            name: '5',
+            img: '../Matchup-Memory-Game/assets/images/5_card.png'
+        },
+        {
+            name: '6',
+            img: '../Matchup-Memory-Game/assets/images/6_card.png'
+        },
+        {
+            name: '6',
+            img: '../Matchup-Memory-Game/assets/images/6_card.png'
+        },
+        {
+            name: 'blue',
+            img: '../Matchup-Memory-Game/assets/images/blue_card.png'
+        },
+        {
+            name: 'blue',
+            img: '../Matchup-Memory-Game/assets/images/blue_card.png'
+        },
+        {
+            name: 'green',
+            img: '../Matchup-Memory-Game/assets/images/green_card.png'
+        },
+        {
+            name: 'green',
+            img: '../Matchup-Memory-Game/assets/images/green_card.png'
+        },
+        {
+            name: 'pink',
+            img: '../Matchup-Memory-Game/assets/images/pink_card.png'
+        },
+        {
+            name: 'pink',
+            img: '../Matchup-Memory-Game/assets/images/pink_card.png'
+        },
+        {
+            name: 'purple',
+            img: '../Matchup-Memory-Game/assets/images/purple_card.png'
+        },
+        {
+            name: 'purple',
+            img: '../Matchup-Memory-Game/assets/images/purple_card.png'
+        },
+        {
+            name: 'red',
+            img: '../Matchup-Memory-Game/assets/images/red_card.png'
+        },
+        {
+            name: 'red',
+            img: '../Matchup-Memory-Game/assets/images/red_card.png'
+        },
+        {
+            name: 'yellow',
+            img: '../Matchup-Memory-Game/assets/images/yellow_card.png'
+        },
+        {
+            name: 'yellow',
+            img: '../Matchup-Memory-Game/assets/images/yellow_card.png'
+        },
+    ]
+
+    // Clear Grid
+    $('#grid').html("");
+
+    // Select Grid
+    let grid3 = $('#grid');
+    $(grid3).attr('class', 'grid3');
+
+    // Score Display
+    let scoreDisplay = document.getElementById('score');
+    scoreDisplay.textContent = ` 0/${cardArray.length/2}`;
+
+    let selectedCards = [];
+    let selectedCardsId = [];
+    let cardsMatched = [];
+
+    // Create Board
+    function createBoard() {
+        for (let i = 0; i < cardArray.length; i++) {
+            let card = document.createElement('img');
+            $(card).attr({
+                'src': '../Matchup-Memory-Game/assets/images/star_card.png',
+                'data-id': i
+            });
+            card.addEventListener('click', flipcard);
+            $(grid3).append(card);
+        }
+    }
+
+    // Card Flip
+    function flipcard() {
+        let cardId = this.getAttribute('data-id');
+        selectedCards.push(cardArray[cardId].name);
+        selectedCardsId.push(cardId);
+        this.setAttribute('src', cardArray[cardId].img);
+        if (selectedCards.length === 2) {
+            setTimeout(checkCards, 500);
+        }
+    }
+
+    createBoard()
+
+    // Check Cards
+    function checkCards() {
+        let cards = document.getElementsByTagName('img');
+        let choiceOneId = selectedCardsId[0];
+        let choiceTwoId = selectedCardsId[1];
+        if (selectedCards[0] === selectedCards[1]) {
+            cards[choiceOneId].setAttribute('src', '../Matchup-Memory-Game/assets/images/blank_card.png');
+            cards[choiceTwoId].setAttribute('src', '../Matchup-Memory-Game/assets/images/blank_card.png');
+            cardsMatched.push(selectedCards);
+            scoreDisplay.textContent = ` ${cardsMatched.length}/${cardArray.length/2}`;
+            cards[choiceOneId].removeEventListener('click', flipcard);
+            cards[choiceTwoId].removeEventListener('click', flipcard);
+            alert('You found a match!')
+        } else {
+            cards[choiceOneId].setAttribute('src', '../Matchup-Memory-Game/assets/images/star_card.png');
+            cards[choiceTwoId].setAttribute('src', '../Matchup-Memory-Game/assets/images/star_card.png');
+            alert('Wrong match! Try again!')
+        }
+        selectedCards = [];
+        selectedCardsId = [];
+
+        if (cardsMatched.length === cardArray.length / 2) {
+            scoreDisplay.textContent = " Congratulations! You've completed difficulty level 3!";
         }
     }
 
