8e10b0d60b71d5276b51f79299538b22d6c3dfbd
Benjamin Brocklebank <82475611+BBrocklebank@users.noreply.github.com>
2021-11-18 11:41:03 +0000

    Feat: Added modal email submission to index and contact pages

--- assets/js/email.js
+++ assets/js/email.js
@@ -7,9 +7,28 @@ function sendMail(contactForm) {
         .then(
             function (response) {
                 console.log("SUCCESS", response);
+                alert('Submission successful, thank you for your feeback.');
             },
             function (error) {
                 console.log("FAILED", error);
+                alert('An error has occured, please try again.');
+            }
+        );
+    return false; // To block from loading a new page
+}
+
+function submission(submitRequest) {
+    emailjs.send("gmail", "matchup-signup", {
+            "from_email": submitRequest.emailaddress.value,
+        })
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+                alert('Submission successful, thank you for your signing up.');
+            },
+            function (error) {
+                console.log("FAILED", error);
+                alert('An error has occured, please try again.');
             }
         );
     return false; // To block from loading a new page
